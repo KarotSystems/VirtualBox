6d3d79efe7ef5153bb79c480d5a47ade98b6a705
André Velasco <83415790+KarotSystems@users.noreply.github.com>
2024-05-08 09:10:03 -0600

    Actualizacion del Main
    Agregar datos sobre la institución donde estoy actualmente.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 print{Mario André Velasco Gonzalez}
 print{Carnet: 1546124}
+
+print{Universidad Rafael Landivar}
+print{Busca la excelencia académica, que significa una calidad superior de ideas, 
+principios y actuaciones de quienes, como profesionales, administrativos y alumnos, 
+puedan situarse por encima de un simple cumplimiento rutinario de su deber.}
+print{Carreras disponibles en esta Universidad}
+print{Licenciatura en Administración de Empresas.}
+print{Licenciatura en Contaduría Pública y Auditoría.}
+print{Licenciatura en Administración de Hoteles y Restaurantes.}
+print{Licenciatura En Artes Culinarias y Negocios Gastronómicos.}
+print{Licenciatura en Innovación y Negocios Sostenibles.}
+print{etc.}
